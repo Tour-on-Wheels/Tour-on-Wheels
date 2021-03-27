a47e929fd01d333214162efd86c3b8fa581070b6
Dipen Kumar <kr.dipen4@gmail.com>
2021-03-27 18:34:12 +0530

    update

--- data/load.sql
+++ data/load.sql
@@ -26,3 +26,28 @@ CREATE TABLE stations (
 
 \copy stations from '/home/manoj/Desktop/sem_6/COL362-dbms/project/project/data/stations.csv' delimiter ',' csv header;
 
+CREATE TABLE trains (
+	third_ac int,
+	arrival time,
+	from_station_code text,
+	name text,
+	zone text,
+	chair_car int,
+	first_class int,
+	duration_m int,
+	sleeper int,
+	from_station_name yext,
+	number int PRIMARY KEY,
+	departure time,
+	return_train int,
+	to_station_code text,
+	second_ac int,
+	classes text,
+	to_station_name text,
+	duration_h int,
+	type text,
+	first_ac int,
+	distance int
+);
+
+\copy stations from '/home/manoj/Desktop/sem_6/COL362-dbms/project/project/data/trains.csv' delimiter ',' csv header;
