bb0e44f3071708108da4a91391feaf447dc697e8
Manoj Kumar <cs5180411@iitd.ac.in>
2021-04-03 17:58:03 +0530

    app.py updated, frontend updated

--- app.py
+++ app.py
@@ -13,10 +13,10 @@ import os
 # cursor = connection.cursor()
 
 connection = psycopg2.connect(
-    host = "127.0.0.1",
-    database = "irctc_db",
-    user = "krdipen",
-    password = "postgres",
+    host = "localhost",
+    database = "railway",
+    user = "postgres",
+    password = "1907",
     port = 5432
 )
 cursor = connection.cursor()
@@ -29,11 +29,17 @@ def index():
     if(request.method == 'POST'):
         task_content1 = request.form['source']
         task_content2 = request.form['destination']
-        tasks = db.engine.execute(text(f"SELECT s1.arrival, s1.departure, s1.train_name, s1.train_number, s2.arrival FROM schedules as s1, schedules as s2 where s1.station_name = '{task_content1}' AND s2.station_name = '{task_content1}' and s1.train_number = s2.train_number ORDER BY s1.arrival" ))
+        cursor.execute(f"SELECT s1.arrival, s1.departure, s1.train_name, s1.train_number, s2.arrival FROM schedules as s1, schedules as s2 where s1.station_name = '{task_content1}' AND s2.station_name = '{task_content1}' and s1.train_number = s2.train_number ORDER BY s1.arrival;" )
+        tasks = cursor.fetchall()
+        print(type(tasks[0]))
         return render_template('index.html', tasks = tasks, src = task_content1, dest = task_content2)
     elif(request.method == 'GET'):
     #     tasks = Todo.query.order_by(Todo.date_created).all()
         return render_template('index.html')
+
+# @app.route('/booking/<list:task>')
+# def booking(task):
+#     return render_template('booking.html', task = task)
 
 if __name__ == "__main__":
     app.run(debug=True)
